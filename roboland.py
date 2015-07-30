af17a74f05a827d62c024c853ebee72de2f1f424
Midaysa Palacios <mida.palacios@gmail.com>
2015-07-30 01:15:58 +0000

    # Use infinite loop for gameplay

---
+++ roboland.py
@@ -0,0 +1,88 @@
+import pprint, os
+WIDTH = 10
+HEIGHT = 10
+
+class Robot():
+    """ Robots gone wild! """
+    
+    def __init__(self, x, y, name, code):
+        """ This method is called whenever a Robot instance is created """
+        self.x = x
+        self.y = y
+        self.name = name
+        self.code = code
+        
+    def __repr__(self):
+        """ Print the robot's attributes """
+        return "Name: " + str(self.name) + ", Coord x: " + str(self.x) + \
+        ", Coord y: " + str(self.y) + ", Code: " +  str(self.code)
+    
+    def move(self, direction):
+        """ Moves the robot in the given direction """
+        direction = direction.lower()
+        
+        if direction == "up" and self.y > 0:
+            self.y -= 1
+        elif direction == "down" and self.y < HEIGHT:
+            self.y += 1
+        elif direction == "left" and self.x > 0:
+            self.x -= 1
+        elif direction == "right" and self.x < WIDTH:
+            self.x += 1
+        else: 
+            print "CAUTION! The robot almost fall from the board"
+            
+    
+
+# List comprehension to build the game board
+board = [[0 for x in range(WIDTH)] for y in range(HEIGHT)]
+
+
+def print_board(board):
+    """ Prints the board using python's pretty printing """
+    pp = pprint.PrettyPrinter(indent = 2)
+    pp.pprint(board)
+    print 
+    
+def place(robot, board):
+    """ Places the robot's code on the board in its x,y coordinate """
+    board[robot.y][robot.x] = robot.code
+    
+def update_board(board, robot_list):
+    """ Update robots positions """
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            board[x][y] = 0
+            
+    for robot in robot_list:
+        place(robot, board)
+        
+
+
+# Infinite game loop
+robot1 = Robot(1,1,"Mida",1)
+robot2 = Robot(1,2,"Piero",2)
+
+while True:
+    place(robot1, board)
+    place(robot2, board)
+    os.system('clear')
+    print robot1
+    print robot2
+    print_board(board)
+    robot1.move("up")
+    robot2.move("up")
+    update_board(board, [robot1, robot2])
+    print robot1
+    print robot2
+    print_board(board)
+    robot1.move("up")
+    robot2.move("right")
+    update_board(board, [robot1, robot2])
+    print robot1
+    print robot2
+    print_board(board)
+    
+    
+    break    
+    
